43377a9f837de6b36220cba7dfbf584e5e66be06
Asterisk2410 <nimit19bhinde98@gmail.com>
2024-04-26 12:46:53 +0530

    chatbot

---
+++ app.py
@@ -0,0 +1,7 @@
+# from src.helper import download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+import os
